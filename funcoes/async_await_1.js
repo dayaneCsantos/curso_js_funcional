492021d6fc51fc38fb75892de2a225d2e38f7f00
Dayane Santos <dayaneday.dd65@gmail.com>
2021-07-25 10:01:26 -0300

    study async/await-2

--- funcoes/async_await_1.js
+++ funcoes/async_await_1.js
@@ -1,8 +1,45 @@
 function esperarPor(tempo = 2000) {
   return new Promise(function (resolve) {
-    setTimeout(function () {
-      console.log('Executando promise...')
-      resolve()
-    }, tempo)
+    setTimeout(() => resolve(), tempo)
   })
 }
+
+// esperarPor()
+//   .then(() => console.log('Executando promise 1...'))
+//   .then(esperarPor)
+//   .then(() => console.log('Executando promise 2...'))
+//   .then(esperarPor)
+//   .then(() => console.log('Executando promise 3...'))
+
+function retornarValor() {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(10), 5000)
+  })
+}
+
+async function retornarRapido() {
+  return 20
+}
+
+async function executar() {
+
+  let valor = await retornarRapido()
+
+  await esperarPor()
+  console.log(`Async/Await ${valor}...`)
+  await esperarPor()
+  console.log(`Async/Await ${valor + 1}...`)
+  await esperarPor()
+  console.log(`Async/Await ${valor + 2}...`)
+
+  return valor + 3
+}
+
+async function executarDeVerdade() {
+  const valor = await executar()
+  console.log(valor)
+}
+executarDeVerdade()
+// const c = executar()
+// console.log(c)
+// executar().then(console.log)
